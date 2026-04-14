f8f45df4555e1a99862090c6b2af5eaa4d348c83
Bolmog1 <defontaines32@gmail.com>
2026-04-14 14:45:51 +0200

    Ajout de la page `/ingredient` Modification du template `base.html` pour pouvoir ajouter du CSS/JS plus facilement

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/ingredient", methods=["GET", "POST"])
+def ingredient():
+    if request.method == 'GET':
+        return render_template("ingredient.html")
+    elif request.method == 'POST':
+        return "Not implemented yet"
